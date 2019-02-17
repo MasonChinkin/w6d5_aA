c5115bcf69e93b9cc7ad8af49f7210a545c210c5
Mason Chinkin <mason.chinkin@gmail.com>
2019-02-16 18:13:47 -0800

    Tabs.jsx finished, but hardcoded click function

--- src/tabs.jsx
+++ src/tabs.jsx
@@ -4,32 +4,30 @@ class Tabs extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { selectedTabIdx: 0 };
+
+		this.clickTab = this.clickTab.bind(this);
 	}
 
 	render() {
 		let i = this.state.selectedTabIdx
-		let title = this.props.tabsData[i].title;
 		let content = this.props.tabsData[i].content;
 		return (
 			<div className="tabs-widget">
 				<h1 className="tab-title">Tabs</h1>
 				<div className="tab-headers">
-					<h1 onClick={clickTab}>{this.props.tabsData[0].title}</h1>
-					<h1 onClick={clickTab}>{this.props.tabsData[1].title}</h1>
-					<h1 onClick={clickTab}>{this.props.tabsData[2].title}</h1>
-
-					{/* <h1>{this.props.tabsData[0].title}</h1>
-                    <h1>{this.props.tabsData[1].title}</h1>
-                    <h1>{this.props.tabsData[2].title}</h1> */}
+					<h1 onClick={() => this.clickTab(0)}>{this.props.tabsData[0].title}</h1>
+					<h1 onClick={() => this.clickTab(1)}>{this.props.tabsData[1].title}</h1>
+					<h1 onClick={() => this.clickTab(2)}>{this.props.tabsData[2].title}</h1>
 				</div>
 				<div className="tab-content">
-					<img src={content} alt="" srcset="" />
+					<img src={content} />
 				</div>
 			</div>
 		)
 	}
 
-	clickTab() {
+	clickTab(key) {
+		this.setState({ selectedTabIdx: key })
 	}
 }
 
